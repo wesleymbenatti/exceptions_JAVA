99172d52f62eac7a2f8cdd1af7c41712eecf058e
Wesley Moraes Benatti <wesleymbenatti@gmail.com>
2020-06-18 22:30:29 -0300

    soluçao ruim

--- src/model/entities/Reservation.java
+++ src/model/entities/Reservation.java
@@ -43,9 +43,18 @@ public class Reservation {
 		return TimeUnit.DAYS.convert(diff, TimeUnit.MICROSECONDS);
 	}
 
-	public void updateDates(Date checkIn, Date checkOut) {
+	public String updateDates(Date checkIn, Date checkOut) {
+		Date now = new Date();
+		if (checkIn.before(now) || checkOut.before(now)) {
+			return "Check-out date must be after check-in date";
+		}
+		else if (!checkOut.after(checkIn)) {
+			return "Check-out date must be after check-in date";
+		}
+		
 		this.checkIn = checkIn;
 		this.checkOut = checkOut;
+		return null;
 	}
 
 	@Override
